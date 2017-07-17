94810604be383d431bf7eb08c05b86d2d10ebad0
Ken Vilar <kenvilar@gmail.com>
2017-07-18 03:27:17 +0800

    Add header

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The theme header
+ *
+ * @package WordPress
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+</head>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html__( 'Skip to content', 'matuto' ); ?></a>
